cf8d76de5c8ea10273007d6eafd638a88afedc96
Abubokkor98 <mail.abubokkor@gmail.com>
2025-02-02 23:26:11 +0600

    ADD simple animation to the about section

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function About() {
   return (
     <section id="about" className="py-20 text-gray-800 bg-gray-50 px-4 sm:px-20">
       <header className="mb-12">
-        <h2 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500">
+        <motion.h2 
+          initial={{ opacity: 0, y: 20 }} 
+          animate={{ opacity: 1, y: 0 }} 
+          transition={{ duration: 0.8 }}
+          className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500"
+        >
           About Me
-        </h2>
+        </motion.h2>
       </header>
 
       {/* About Content */}
-      <article className="text-base sm:text-lg leading-relaxed text-gray-700 mb-6 w-full md:w-10/12">
+      <motion.article 
+        initial={{ opacity: 0, y: 20 }} 
+        animate={{ opacity: 1, y: 0 }} 
+        transition={{ duration: 1, delay: 0.3 }}
+        className="text-base sm:text-lg leading-relaxed text-gray-700 text-left mb-6 w-full lg:w-10/12"
+      >
         <p>
           Hi, I'm <strong>Abu Bokkor Siddik</strong>, a passionate{" "}
           <strong>Front-End Developer</strong> who loves building interactive, user-friendly, and scalable web applications. 
@@ -18,7 +29,7 @@ export default function About() {
         </p>
 
         <p className="mt-4">
-        I hold a <strong>BSc in Computer Science and Engineering</strong> from  
+          I hold a <strong>BSc in Computer Science and Engineering</strong> from  
           <strong> Shaikh Burhanuddin Post Graduate College, Dhaka</strong> (Graduated: 2023). 
           My academic journey strengthened my skills in <strong>problem-solving, software development, and system design</strong>, 
           shaping my approach to building efficient applications.
@@ -34,7 +45,7 @@ export default function About() {
           Always eager to learn, I actively explore modern web technologies and best practices to stay ahead in the field. 
           My goal is to contribute to impactful projects that deliver seamless digital experiences.
         </p>
-      </article>
+      </motion.article>
     </section>
   );
 }
